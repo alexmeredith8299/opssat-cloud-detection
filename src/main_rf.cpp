11c313d66d78c1ef5305aaed8cec3ef309c7eb56
Alex Meredith <ameredit@mit.edu>
2022-02-22 15:47:21 -0800

    still have to add a bash script for whole rf pipeline (and fix makefile for ranger), but have all the pieces for img -> csv -> ranger -> csv -> img

--- src/main_rf.cpp
+++ src/main_rf.cpp
@@ -20,6 +20,7 @@
 #include <vector>
 #include <iterator>
 #include <csv2/writer.hpp>
+#include <csv2/reader.hpp>
 
 #define INPUT_CSV_FILENAME  "ranger_input.csv"
 
@@ -31,11 +32,12 @@ using namespace csv2;
 using std::string;
 // using cstd
 
-int parse_options(int argc, char **argv, string *img_write_path, int *img_write_mode)
+int parse_options(int argc, char **argv, string *img_input_path, int *file_mode, string *img_output_path)
 {
     /* get provider host and port from command arguments */
     int8_t argv_index_input = -1;
-    int8_t argv_index_write = -1;
+    int8_t argv_index_mode = -1;
+    int8_t argv_index_output = -1;
 
     // --------------------------------------------------------------------------
     // parse the command arguments
@@ -45,13 +47,12 @@ int parse_options(int argc, char **argv, string *img_write_path, int *img_write_
     {
         if (streq(argv[argn], "--help") || streq(argv[argn], "-?"))
         {
-            printf("io_demo [options] ...");
-            printf("\n  --input    / -i       the file path of the input image");
-            printf("\n  --write    / -w       the write mode of the output image (optional)"
-                   "\n\t0 - do not write a new image (equivalent to not specifying the --write option)"
-                   "\n\t1 - write a new image as a new file"
-                   "\n\t2 - write a new image that overwrites the input image file"
-                   "\n\t3 - same as option 2 but backs up the original input image");
+            printf("opssat_rf_segment [options] ...");
+            printf("\n  --input    / -i       the file path of the input image or csv");
+            printf("\n  --mode    / -m     the file conversion mode (img -> csv or csv -> img)"
+                   "\n\t1 - image to csv (equivalent to not specifying the --write option)"
+                   "\n\t2 - csv to image");
+            printf("\n  --output   / -o   the output file name (optional)");
             printf("\n  --help     / -?       this information\n");
 
             /* program error exit code */
@@ -60,8 +61,10 @@ int parse_options(int argc, char **argv, string *img_write_path, int *img_write_
         }
         else if (streq(argv[argn], "--input") || streq(argv[argn], "-i"))
             argv_index_input = ++argn;
-        else if (streq(argv[argn], "--write") || streq(argv[argn], "-w"))
-            argv_index_write = ++argn;
+        else if (streq(argv[argn], "--mode") || streq(argv[argn], "-m"))
+            argv_index_mode = ++argn;
+        else if (streq(argv[argn], "--output") || streq(argv[argn], "-o"))
+            argv_index_output = ++argn;
         else
         {
             /* print error message */
@@ -79,7 +82,7 @@ int parse_options(int argc, char **argv, string *img_write_path, int *img_write_
     if (argv_index_input == -1)
     {
         /* print error message */
-        printf("no image input path specified. Get help: ./io_demo -?\n");
+        printf("no image input path specified. Get help: opssat_rf_segment --help?\n");
 
         /* program error exit code */
         /* 22 	EINVAL 	Invalid argument */
@@ -89,54 +92,62 @@ int parse_options(int argc, char **argv, string *img_write_path, int *img_write_
     {
         /* printf for documentation purposes only */
         printf("image to process: %s\n", argv[argv_index_input]);
-        *img_write_path = string(argv[argv_index_input]);
+        *img_input_path = string(argv[argv_index_input]);
     }
 
-    // --------------------------------------------------------------------------
-    // parse the output image write mode option, if given
-    if (argv_index_write == -1)
+    if(argv_index_mode == -1)
     {
-        /* printf for documentation purposes only */
-        printf("no output image will be written\n");
+      /* print error message */
+      printf("no image mode specified. Get help: opssat_rf_segment --help?\n");
+
+      /* program error exit code */
+      /* 22 	EINVAL 	Invalid argument */
+      return EINVAL;
     }
     else
     {
-        /* parse write mode option string to int */
-        int8_t write_mode = atoi(argv[argv_index_write]);
+      /* parse write mode option string to int */
+      int8_t mode = atoi(argv[argv_index_mode]);
 
-        // set to write mode
-        // *img_write_mode = write_mode;
-        printf("write mode %i\n", write_mode);
-        /* prompt which option was selected */
-        switch (write_mode)
-        {
-        case 0:
-            /* printf for documentation purposes only */
-            printf("don't write a new image (equivalent to not specifying the --write option)\n");
-            break;
+      // set to write mode
+      // *img_write_mode = write_mode;
+      printf("file conversion mode %i\n", mode);
+      /* prompt which option was selected */
+      switch (mode)
+      {
+      case 1:
+          /* printf for documentation purposes only */
+          *file_mode = mode;
+          printf("image to csv \n");
+          break;
 
-        case 1:
-            /* printf for documentation purposes only */
-            printf("write a new image as a new file\n");
-            break;
+      case 2:
+          /* printf for documentation purposes only */
+          *file_mode = mode;
+          printf("csv to image \n");
+          break;
+      default:
+          /* print error message */
+          printf("invalid  mode option. Get help: opssat_rf_segment --help?\n");
+          /* program error exit code */
+          /* 22 	EINVAL 	Invalid argument */
+          return EINVAL;
+      }
+    }
 
-        case 2:
-            /* printf for documentation purposes only */
-            printf("write a new image that overwrites the input image file\n");
-            break;
 
-        case 3:
-            /* printf for documentation purposes only */
-            printf("write a new image that overwrites the input image file but backs up the original input image\n");
-            break;
-
-        default:
-            /* print error message */
-            printf("invalid write mode option. Get help: ./io_demo -?\n");
-            /* program error exit code */
-            /* 22 	EINVAL 	Invalid argument */
-            return EINVAL;
-        }
+    // --------------------------------------------------------------------------
+    // parse the output image write mode option, if given
+    if (argv_index_output == -1)
+    {
+        /* printf for documentation purposes only */
+        printf("no output filename specified, using default (out.csv or out.png)\n");
+    }
+    else
+    {
+        /* printf for documentation purposes only */
+        printf("file output path: %s\n", argv[argv_index_output]);
+        *img_output_path = string(argv[argv_index_output]);
     }
 
     return 0;
@@ -147,7 +158,7 @@ void log_vitals()
     throw "not implemented";
 }
 
-void write_img_to_csv(std::string img_path)
+void write_img_to_csv(std::string img_path, std::string csv_path)
 {
   int width, height, channels;
   // use 0 to have stb figure out components per pixel
@@ -200,7 +211,7 @@ void write_img_to_csv(std::string img_path)
 
 
 
-  std::ofstream stream("foo.csv");
+  std::ofstream stream(csv_path);
     Writer<delimiter<','>> writer(stream);
 
     /*std::vector<std::vector<std::string>> rows =
@@ -232,9 +243,13 @@ void write_img_to_csv(std::string img_path)
     headers.push_back("CLOUD");
     rows.push_back(headers);
 
-    for (int i = 0; i < width; i++)
+    //RBF: used so our csv isn't obsecenly giant
+    int i_start = width-200;
+    int j_start = height-200;
+
+    for (int i = i_start; i < width; i++)
     {
-        for (int j = 0; j < height; j++)
+        for (int j = j_start; j < height; j++)
         {
             // memory offset from [0]
             // row major indexing
@@ -244,7 +259,8 @@ void write_img_to_csv(std::string img_path)
             int r_22 = img[offset];
             int g_22 = img[offset + 1];
             int b_22 = img[offset + 2];
-            //std::cout<<"R, G, B="<<r_px<<", "<<g_px<<", "<<b_px<<"\n";
+            std::cout<<"R, G, B="<<r_22<<", "<<g_22<<", "<<b_22<<"\n";
+            std::cout<<"Offset "<<offset<<"\n";
 
             int jm = j-1;
             int jp = j+1;
@@ -362,27 +378,111 @@ void write_img_to_csv(std::string img_path)
     writer.write_rows(rows);
     stream.close();
 
-    printf("writing image...\n");
-    stbi_write_png("yee.png", width, height, channels, luminosity_out, width * channels);
+    //printf("writing image...\n");
+    //stbi_write_png("yee.png", width, height, channels, luminosity_out, width * channels);
 
     // remember to free the image at the very end
     stbi_image_free(img);
     stbi_image_free(luminosity_out);
 }
 
+void write_csv_to_img(std::string csv_path, std::string img_path)
+{
+  csv2::Reader<delimiter<','>,
+               quote_character<'"'>,
+               first_row_is_header<true>,
+               trim_policy::trim_whitespace> csv;
+
+  if (csv.mmap(csv_path)) {
+    const auto header = csv.header();
+    for( const auto cell: header) {
+      std::string val;
+      cell.read_value(val);
+      std::cout<<val<<"\n";
+    }
+    int height = 200;
+    int width = 200;
+    int channels = 3; //R, G, B
+
+    int img_memory = sizeof(uint8_t) * width * height * channels;
+    // TODO: error check, if !img etc
+    // printf("image loaded\n");
+    // luminosity based implementation - let's loop through every pixel
+    // first allocate an output image...
+    uint8_t *cloud_mask_out = (uint8_t *)malloc(img_memory);
+    printf("%i\n bytes allocated", img_memory);
+
+    int csv_row= 0;
+    size_t nrows = csv.rows();
+    std::cout<<"nrows="<<nrows;
+    for (const auto row: csv) {
+      for (const auto cell: row) {
+        //Read cell value
+        std::string val;
+        cell.read_value(val);
+        if (csv_row >= 2) {
+          //int offset = (channels) * ((width * j) + i);
+          //int r_px = img[offset];
+          //int g_px = img[offset + 1];
+          //int b_px = img[offset + 2];
+
+          int idx = channels*(csv_row-2);
+          std::cout<<"idx, val, ht="<<idx<<", "<<idx<<", "<<csv_row<<"\n";
+
+          if (val == "1")
+          {
+            cloud_mask_out[idx] = 255;
+            cloud_mask_out[idx+1] = 255;
+            cloud_mask_out[idx+2] = 255;
+          } else
+          {
+            cloud_mask_out[idx] = 0;
+            cloud_mask_out[idx+1] = 0;
+            cloud_mask_out[idx+2] = 0;
+          }
+        }
+      }
+      csv_row++;
+    }
+    std::cout<<"w, h = "<<width<<","<<height;
+    printf("writing image...\n");
+    const char* img_outpath = img_path.c_str();
+    stbi_write_png(img_outpath, width, height, channels, cloud_mask_out, width * channels);
+
+    // remember to free the image at the very end
+    stbi_image_free(cloud_mask_out);
+  }
+}
+
 int main(int argc, char **argv)
 {
-    int write_mode;
-    // 0 - do not write new img
-    // 1 - write new image
-    // 2 - overwrite original image
-    // 3 - overwrite but back up original
-    string img_path;
+    int mode;
+    // 0 - img to csv
+    // 1 - csv to img
+    string input_path;
+    string output_path;
 
-    parse_options(argc, argv, &img_path, &write_mode);
+    parse_options(argc, argv, &input_path, &mode, &output_path);
 
-    printf("getting here\n");
-    // try loading an image
-    // heihgt, width, number of components
-    write_img_to_csv(img_path);
+    std::cout<<"here\n";
+    std::cout<<"mode"<<mode<<"\n";
+
+    if(input_path.length() != 0) {
+      if(mode == 1) {
+        if(output_path.length() == 0) {
+          write_img_to_csv(input_path, "out.csv");
+        }
+        else {
+          write_img_to_csv(input_path, output_path);
+        }
+      } else if(mode == 2) {
+        std::cout<<"here\n";
+        if(output_path.length() == 0) {
+          write_csv_to_img(input_path, "out.png");
+        }
+        else {
+          write_csv_to_img(input_path, output_path);
+        }
+      }
+    }
 }
